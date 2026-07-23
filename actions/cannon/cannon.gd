576fbeda07e47565ed64009d8a837c38a307c7be
Void-Foxy <ryan.h0386@gmail.com>
2026-07-23 18:46:36 -0400

    cannon updates

--- actions/cannon/cannon.gd
+++ actions/cannon/cannon.gd
@@ -3,7 +3,9 @@ extends Area2D
 @export var barrel : Node2D
 @export var barrelTip : Node2D
 
-var thingInCannnon : RigidBody2D
+var thingInCannon : RigidBody2D
+var dir : Vector2
+@export var shootingForce : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,21 +14,26 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if thingInCannnon != null && Input.is_action_just_pressed("cannon action"):
+	if thingInCannon != null && Input.is_action_just_pressed("cannon action"):
 		fire()
 	pass
 
 
 func setUpCannon() -> void:
 	barrel.look_at(get_global_mouse_position())
+	dir = barrelTip.global_position - self.global_position
 
 func fire() -> void:
+	print("firing")
+	thingInCannon.freeze = false
+	thingInCannon.apply_impulse(dir * shootingForce)
 	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("things that can go in a cannon"):
-		thingInCannnon = body
-		thingInCannnon.freeze
-		thingInCannnon.global_position = barrelTip.position
+		thingInCannon = body
+		thingInCannon.freeze = true
+		thingInCannon.global_position = barrelTip.global_position
+		
 	pass # Replace with function body.
--- actions/explosion orb/explosion_orb.gd
+++ actions/explosion orb/explosion_orb.gd
@@ -5,7 +5,6 @@ var explosionRad : Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	add_to_group("things that can go in a cannon")
 	explosionRad =  $Area2D
 	pass # Replace with function body.
 
