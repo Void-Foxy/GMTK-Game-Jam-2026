a6c7a860317ab7075045f6665d60439c8834e2eb
Subhang <nandurisubhang9@gmail.com>
2026-07-25 15:45:32 -0400

    fixed sword action

--- player/states/default.gd
+++ player/states/default.gd
@@ -6,7 +6,7 @@ func _integration_state_logic(physics_state: PhysicsDirectBodyState2D) -> void:
 
 
 func _get_transition(_delta: float) -> State:
-	var sword_action := "sword_action"
+	var sword_action := "sword action"
 	if fsm_owner.scm.action_container.just_pressed(sword_action, true):
 		fsm_owner.scm.action_container.use_action_slot(sword_action)
 		return states.SwordVerticalSlash
