8353fa10d80990090d80c54167d7348b79a9cf93
Void-Foxy <ryan.h0386@gmail.com>
2026-07-25 13:01:30 -0400

    arrows keep rotation when stuck in object

--- actions/bow/arrow.gd
+++ actions/bow/arrow.gd
@@ -1,20 +1,25 @@
 extends RigidBody2D
 
 var stuckIn := false
+var sprite : Sprite2D
+var lastFrameDir : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	sprite = $Arrow
 	body_entered.connect(_on_body_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if !stuckIn:
-		look_at(global_position + linear_velocity)
+		lastFrameDir = sprite.rotation
+		sprite.look_at(global_position + linear_velocity)
 	pass
 
 func _on_body_entered(body: Node) -> void:
 	if !body.is_in_group("player"):
+		sprite.rotation = lastFrameDir
 		stuckIn = true
 		call_deferred("set_freeze_enabled", true)
 		$CollisionShape2D.set_deferred("disabled", true)
