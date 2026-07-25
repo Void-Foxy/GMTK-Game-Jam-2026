c9f1daa21ce9641638fc80fcbb07db7f1cb0b8f5
Subhang <nandurisubhang9@gmail.com>
2026-07-25 14:58:56 -0400

    some level thing

--- helpers/global.gd
+++ helpers/global.gd
@@ -8,9 +8,19 @@ signal level_ready
 
 var throwables : Node2D:
 	get: return level.throwables
-var explosiveExist := false
-var teleportExist := false
-var cannonExist := false
+var explosiveExist: bool:
+	set(value):
+		level.explosiveExist = value
+	get: return level.explosiveExist
+var teleportExist: bool:
+	set(value):
+		level.teleportExist = value
+	get: return level.teleportExist
+
+var cannonExist: bool:
+	set(value):
+		level.cannonExist = value
+	get: return level.cannonExist
 
 func emit_level_ready() -> void:
 	level_ready.emit()
