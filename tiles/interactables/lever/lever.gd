5a64c4d2dd9b2046d83959506625cd6dc4f7fbec
Subhang <nandurisubhang9@gmail.com>
2026-07-25 21:12:00 -0400

    level 1 complete

--- tiles/interactables/lever/lever.gd
+++ tiles/interactables/lever/lever.gd
@@ -12,5 +12,7 @@ func interaction_output() -> void:
 	toggled_on = !toggled_on
 	output.emit(toggled_on)
 
-func _on_body_entered(_body: Node2D) -> void:
+func _on_body_entered(body: Node2D) -> void:
+	if body is TileMapLayer:
+		return
 	interaction_output()
