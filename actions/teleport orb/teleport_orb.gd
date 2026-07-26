d9e248d85e8024c97ebe8a69b91de4491b5ca761
Subhang <nandurisubhang9@gmail.com>
2026-07-26 00:29:54 -0400

    Teleport orb now deletes itself when hitting player collider wall

--- actions/teleport orb/teleport_orb.gd
+++ actions/teleport orb/teleport_orb.gd
@@ -4,11 +4,13 @@ var player : Player
 var other : RigidBody2D
 
 @export var area2D : Area2D
+@export var fizzler: Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_to_group("things that can go in a cannon")
 	area2D.body_entered.connect(_on_body_entered)
+	fizzler.body_entered.connect(_on_fizzler_body_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -38,16 +40,25 @@ func teleport() -> void:
 	Global.teleportExist = false
 	queue_free()
 
+func get_destroyed() -> void:
+	Global.teleportExist = false
+	queue_free()
+
+func _on_fizzler_body_entered(body: Node) -> void:
+	if body is TileMapLayer:
+		get_destroyed()
 
 func _on_body_entered(body: Node) -> void:
 	if body == self:
 		return
 	if body.is_in_group("player"):
 		return
-	print("Body: ", body)
+	
 	call_deferred("set_freeze_enabled", true)
 	if body.is_in_group("teleportable"):
-		$CollisionShape2D.set_deferred("disabled", true)
+		collision_mask -= 2**(2-1)
+		
+		#$CollisionShape2D.set_deferred("disabled", true)
 		other = body
 		linear_velocity = Vector2.ZERO
 		angular_velocity = 0
